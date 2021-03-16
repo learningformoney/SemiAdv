433af1901ea9f31301fa26708470770c45fc5c1d
learningformoney <67225752+learningformoney@users.noreply.github.com>
2021-03-16 12:36:09 +0800

    Add files via upload

---
+++ train_cifar.py
@@ -0,0 +1,375 @@
+import os
+import shutil
+import time
+import random
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+
+import models.googlenet as BlackModel
+import models.wideresnet as models
+import dataset.cifar10 as dataset
+from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
+from tensorboardX import SummaryWriter
+
+
+
+# Use CUDA
+use_cuda = torch.cuda.is_available()
+
+
+best_acc = 0  # best test accuracy
+
+def create_model_wideresnet(ema=False):
+    model = models.WideResNet(num_classes=10)
+    model = model.cuda()
+
+    if ema:
+        for param in model.parameters():
+            param.detach_()
+
+    return model
+
+def create_model_efficientnet(ema=False):
+    model = models.EfficientNetB0()
+    model = model.cuda()
+
+    if ema:
+        for param in model.parameters():
+            param.detach_()
+
+    return model
+
+
+def main(create_model, black_model, output_dir, labeled_num):
+    global best_acc
+
+    if not os.path.isdir(output_dir):
+        mkdir_p(output_dir)
+
+    # Data
+    print(f'==> Preparing cifar10')
+    transform_train = transforms.Compose([
+        dataset.RandomPadandCrop(32),
+        dataset.RandomFlip(),
+        dataset.ToTensor(),
+    ])
+
+    transform_val = transforms.Compose([
+        dataset.ToTensor(),
+    ])
+
+    train_labeled_set, train_unlabeled_set, val_set, test_set = dataset.get_cifar10('../data', labeled_num, transform_train=transform_train, transform_val=transform_val)
+    labeled_trainloader = torch.utils.data.DataLoader(train_labeled_set, batch_size=10, shuffle=True, num_workers=0, drop_last=True)
+    unlabeled_trainloader = torch.utils.data.DataLoader(train_unlabeled_set, batch_size=10, shuffle=True, num_workers=0, drop_last=True)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=10, shuffle=False, num_workers=0)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=10, shuffle=False, num_workers=0)
+
+    model = create_model()
+    ema_model = create_model(ema=True)
+
+    cudnn.benchmark = True
+
+    train_criterion = SemiLoss()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.002)
+
+    ema_optimizer= WeightEMA(model, ema_model, alpha=0.999)
+    start_epoch = 0
+
+    # Resume
+    title = 'noisy-cifar-10'
+    logger = Logger(os.path.join(output_dir, 'log.txt'), title=title)
+    logger.set_names(['Train Loss', 'Train Loss X', 'Train Loss U',  'Valid Loss', 'Valid Acc.', 'Test Loss', 'Test Acc.'])
+
+    writer = SummaryWriter(output_dir)
+    step = 0
+    test_accs = []
+    
+    black_model.eval()
+    meancifar, stdcifar = torch.Tensor([0.471, 0.448, 0.408]), torch.Tensor([0.234, 0.239, 0.242])
+    transformcifar = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),torchvision.transforms.Normalize(meancifar,stdcifar)])
+    data = torchvision.datasets.CIFAR10(root="../data",train=False,download=False,transform=transformcifar)
+    loadercifar = torch.utils.data.DataLoader(data,batch_size=32,shuffle=False)
+    # Train and val
+    for epoch in range(start_epoch, 1000):
+
+        train_loss, train_loss_x, train_loss_u = train(labeled_trainloader, unlabeled_trainloader, model, optimizer, ema_optimizer, train_criterion, epoch, use_cuda, black_model)
+        _, train_acc = validate(labeled_trainloader, ema_model, criterion, epoch, use_cuda, mode='Train Stats')
+        val_loss, val_acc = validate(val_loader, ema_model, criterion, epoch, use_cuda, mode='Valid Stats')
+        test_loss, test_acc = validate(test_loader, ema_model, criterion, epoch, use_cuda, mode='Test Stats ')
+
+        step = 1024 * (epoch + 1)
+
+        writer.add_scalar('losses/train_loss', train_loss, step)
+        writer.add_scalar('losses/valid_loss', val_loss, step)
+        writer.add_scalar('losses/test_loss', test_loss, step)
+
+        writer.add_scalar('accuracy/train_acc', train_acc, step)
+        writer.add_scalar('accuracy/val_acc', val_acc, step)
+        writer.add_scalar('accuracy/test_acc', test_acc, step)
+
+        # append logger file
+        logger.append([train_loss, train_loss_x, train_loss_u, val_loss, val_acc, test_loss, test_acc])
+
+        # save model
+        is_best = val_acc > best_acc
+        best_acc = max(val_acc, best_acc)
+        save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+                'ema_state_dict': ema_model.state_dict(),
+                'acc': val_acc,
+                'best_acc': best_acc,
+                'optimizer' : optimizer.state_dict(),
+            }, is_best, output_dir)
+        test_accs.append(test_acc)
+    logger.close()
+    writer.close()
+
+    print('Best acc:')
+    print(best_acc)
+
+    print('Mean acc:')
+    print(np.mean(test_accs[-20:]))
+
+
+def train(labeled_trainloader, unlabeled_trainloader, model, optimizer, ema_optimizer, criterion, epoch, use_cuda, black_model):
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    losses_x = AverageMeter()
+    losses_u = AverageMeter()
+    ws = AverageMeter()
+    end = time.time()
+
+    bar = Bar('Training', max=1024)
+    labeled_train_iter = iter(labeled_trainloader)
+    unlabeled_train_iter = iter(unlabeled_trainloader)
+
+    model.train()
+    for batch_idx in range(1024):
+        try:
+            inputs_x, targets_x = labeled_train_iter.next()
+        except:
+            labeled_train_iter = iter(labeled_trainloader)
+            inputs_x, targets_x = labeled_train_iter.next()
+
+        try:
+            (inputs_u, inputs_u2), _ = unlabeled_train_iter.next()
+        except:
+            unlabeled_train_iter = iter(unlabeled_trainloader)
+            (inputs_u, inputs_u2), _ = unlabeled_train_iter.next()
+
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        batch_size = inputs_x.size(0)
+        inputs_x, targets_x = inputs_x.cuda(), targets_x.cuda()
+        targets_x = black_model(inputs_x).max(dim=1)[1].cpu()
+        # Transform label to one-hot
+        targets_x = torch.zeros(batch_size, 10).scatter_(1, targets_x.view(-1,1).long(), 1)
+
+        if use_cuda:
+            inputs_x, targets_x = inputs_x.cuda(), targets_x.cuda(non_blocking=True)
+            inputs_u = inputs_u.cuda()
+            inputs_u2 = inputs_u2.cuda()
+
+
+        with torch.no_grad():
+            # compute guessed labels of unlabel samples
+            outputs_u = model(inputs_u)
+            outputs_u2 = model(inputs_u2)
+            p = (torch.softmax(outputs_u, dim=1) + torch.softmax(outputs_u2, dim=1)) / 2
+            pt = p**(1/0.5)
+            targets_u = pt / pt.sum(dim=1, keepdim=True)
+            targets_u = targets_u.detach()
+
+        # mixup
+        all_inputs = torch.cat([inputs_x, inputs_u, inputs_u2], dim=0)
+        all_targets = torch.cat([targets_x, targets_u, targets_u], dim=0)
+
+        l = np.random.beta(0.75, 0.75)
+
+        l = max(l, 1-l)
+
+        idx = torch.randperm(all_inputs.size(0))
+
+        input_a, input_b = all_inputs, all_inputs[idx]
+        target_a, target_b = all_targets, all_targets[idx]
+
+        mixed_input = l * input_a + (1 - l) * input_b
+        mixed_target = l * target_a + (1 - l) * target_b
+
+        # interleave labeled and unlabed samples between batches to get correct batchnorm calculation 
+        mixed_input = list(torch.split(mixed_input, batch_size))
+        mixed_input = interleave(mixed_input, batch_size)
+
+        logits = [model(mixed_input[0])]
+        for input in mixed_input[1:]:
+            logits.append(model(input))
+
+        # put interleaved samples back
+        logits = interleave(logits, batch_size)
+        logits_x = logits[0]
+        logits_u = torch.cat(logits[1:], dim=0)
+
+        Lx, Lu, w = criterion(logits_x, mixed_target[:batch_size], logits_u, mixed_target[batch_size:], epoch+batch_idx/1024)
+
+        loss = Lx + w * Lu
+
+        # record loss
+        losses.update(loss.item(), inputs_x.size(0))
+        losses_x.update(Lx.item(), inputs_x.size(0))
+        losses_u.update(Lu.item(), inputs_x.size(0))
+        ws.update(w, inputs_x.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        ema_optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # plot progress
+        bar.suffix  = '({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | Loss_x: {loss_x:.4f} | Loss_u: {loss_u:.4f} | W: {w:.4f}'.format(
+                    batch=batch_idx + 1,
+                    size=1024,
+                    data=data_time.avg,
+                    bt=batch_time.avg,
+                    total=bar.elapsed_td,
+                    eta=bar.eta_td,
+                    loss=losses.avg,
+                    loss_x=losses_x.avg,
+                    loss_u=losses_u.avg,
+                    w=ws.avg,
+                    )
+        bar.next()
+    bar.finish()
+
+    return (losses.avg, losses_x.avg, losses_u.avg,)
+
+def validate(valloader, model, criterion, epoch, use_cuda, mode):
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    bar = Bar(f'{mode}', max=len(valloader))
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(valloader):
+            # measure data loading time
+            data_time.update(time.time() - end)
+
+            if use_cuda:
+                inputs, targets = inputs.cuda(), targets.cuda(non_blocking=True)
+            # compute output
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # measure accuracy and record loss
+            prec1, prec5 = accuracy(outputs, targets, topk=(1, 5))
+            losses.update(loss.item(), inputs.size(0))
+            top1.update(prec1.item(), inputs.size(0))
+            top5.update(prec5.item(), inputs.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            # plot progress
+            bar.suffix  = '({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
+                        batch=batch_idx + 1,
+                        size=len(valloader),
+                        data=data_time.avg,
+                        bt=batch_time.avg,
+                        total=bar.elapsed_td,
+                        eta=bar.eta_td,
+                        loss=losses.avg,
+                        top1=top1.avg,
+                        top5=top5.avg,
+                        )
+            bar.next()
+        bar.finish()
+    return (losses.avg, top1.avg)
+
+def save_checkpoint(state, is_best, checkpoint, filename='checkpoint.pth.tar'):
+    filepath = os.path.join(checkpoint, filename)
+    torch.save(state, filepath)
+    if is_best:
+        shutil.copyfile(filepath, os.path.join(checkpoint, 'model_best.pth.tar'))
+
+def linear_rampup(current, rampup_length=1000):
+    if rampup_length == 0:
+        return 1.0
+    else:
+        current = np.clip(current / rampup_length, 0.0, 1.0)
+        return float(current)
+
+class SemiLoss(object):
+    def __call__(self, outputs_x, targets_x, outputs_u, targets_u, epoch):
+        probs_u = torch.softmax(outputs_u, dim=1)
+
+        Lx = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
+        Lu = torch.mean((probs_u - targets_u)**2)
+
+        return Lx, Lu, 75 * linear_rampup(epoch)
+
+class WeightEMA(object):
+    def __init__(self, model, ema_model, alpha=0.999):
+        self.model = model
+        self.ema_model = ema_model
+        self.alpha = alpha
+        self.params = list(model.state_dict().values())
+        self.ema_params = list(ema_model.state_dict().values())
+        self.wd = 0.02 * 0.002
+
+        for param, ema_param in zip(self.params, self.ema_params):
+            param.data.copy_(ema_param.data)
+
+    def step(self):
+        one_minus_alpha = 1.0 - self.alpha
+        for param, ema_param in zip(self.params, self.ema_params):
+            if ema_param.dtype==torch.float32:
+                ema_param.mul_(self.alpha)
+                ema_param.add_(param * one_minus_alpha)
+                # customized weight decay
+                param.mul_(1 - self.wd)
+
+def interleave_offsets(batch, nu):
+    groups = [batch // (nu + 1)] * (nu + 1)
+    for x in range(batch - sum(groups)):
+        groups[-x - 1] += 1
+    offsets = [0]
+    for g in groups:
+        offsets.append(offsets[-1] + g)
+    assert offsets[-1] == batch
+    return offsets
+
+
+def interleave(xy, batch):
+    nu = len(xy) - 1
+    offsets = interleave_offsets(batch, nu)
+    xy = [[v[offsets[p]:offsets[p + 1]] for p in range(nu + 1)] for v in xy]
+    for i in range(1, nu + 1):
+        xy[0][i], xy[i][i] = xy[i][i], xy[0][i]
+    return [torch.cat(v, dim=0) for v in xy]
+
